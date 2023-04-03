bb28307406a29d6ce7a8bed6d464fffa9f66e023
peregrinno <joseluan74@gmail.com>
2023-04-03 18:43:43 -0300

    Random commit

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ISI-03
 
-Esse código é um sistema simples de gerenciamento de celulares utilizando Node.js e PostgreSQL. A aplicação consiste em uma página HTML (celulares.js) que lista todos os celulares cadastrados no banco de dados, e um servidor Node.js (app.js) que fornece as rotas para criar, ler, atualizar e excluir celulares no banco de dados.
+<h1>Começando pelo começo!</h1>
+
+<p>Esse código é um sistema simples de gerenciamento de celulares utilizando Node.js e PostgreSQL. A aplicação consiste em uma página HTML (celulares.js) que lista todos os celulares cadastrados no banco de dados, e um servidor Node.js (app.js) que fornece as rotas para criar, ler, atualizar e excluir celulares no banco de dados.
 
 A página HTML usa o framework Bootstrap para renderizar a lista de celulares. Cada celular é exibido como um item de lista, com uma imagem, nome, especificações e valor. Para cada celular, há botões para editar e excluir o celular.
 
@@ -10,4 +12,4 @@ Quando o usuário envia um formulário de adição de um novo celular, o servido
 
 As consultas SQL são feitas usando o objeto Pool da biblioteca pg, que gerencia as conexões com o banco de dados. O servidor usa o middleware body-parser para analisar os dados enviados pelo formulário HTTP.
 
-O código HTML usa a sintaxe EJS (Embedded JavaScript) para inserir os dados dos celulares renderizados pelo servidor. A página HTML também usa o JavaScript do Bootstrap para exibir o modal de edição quando o usuário clica no botão de editar um celular.
+O código HTML usa a sintaxe EJS (Embedded JavaScript) para inserir os dados dos celulares renderizados pelo servidor. A página HTML também usa o JavaScript do Bootstrap para exibir o modal de edição quando o usuário clica no botão de editar um celular.<p>
--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const app = express();
 
+// Configuração do servidor
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 // Configuração do banco de dados
 const pool = new Pool({
   user: 'postgres',
@@ -12,11 +17,6 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Configuração do servidor
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
 // Rota principal
 app.get('/', (req, res) => {
   pool.query('SELECT * FROM celulares', (error, results) => {
@@ -25,20 +25,14 @@ app.get('/', (req, res) => {
   });
 });
 
-// Rota para adicionar um novo celular
-app.post('/celulares', (req, res) => {
-  const celular = req.body;
-  pool.query('INSERT INTO celulares (id, nome, valor, imagem, especificacao) VALUES (default, $1, $2, $3, $4)', [celular.nome, celular.valor, celular.imagem, celular.especificacao], (error, results) => {
-    if (error) throw error;
-    res.redirect('/');
-  });
-});
-
 // Rota para atualizar um celular existente
 app.put('/celulares/:id', (req, res) => {
   const id = req.params.id;
   const celular = req.body;
-  pool.query('UPDATE celulares SET nome = $1, valor = $2, imagem =$3, especificacao = $4 WHERE id = $5', [celular.nome, celular.valor, celular.imagem, celular.especificacao, id], (error, results) => {
+  const sql = 'UPDATE celulares SET nome = $1, valor = $2, imagem =$3, especificacao = $4 WHERE id = $5';
+  const values = [celular.nome, celular.valor, celular.imagem, celular.especificacao, id]
+  console.log(sql, values); // imprime a consulta e os valores
+  pool.query(sql, values, (error, results) => {
     if (error) throw error;
     res.redirect('/');
   });
@@ -48,6 +42,15 @@ app.put('/celulares/:id', (req, res) => {
 app.delete('/celulares/:id', (req, res) => {
   const id = req.params.id;
   pool.query('DELETE FROM celulares WHERE id = $1', [id], (error, results) => {
+    if (error) throw error;
+    res.redirect('/');
+  });
+});
+
+// Rota para adicionar um novo celular
+app.post('/celulares', (req, res) => {
+  const celular = req.body;
+  pool.query('INSERT INTO celulares (id, nome, valor, imagem, especificacao) VALUES (default, $1, $2, $3, $4)', [celular.nome, celular.valor, celular.imagem, celular.especificacao], (error, results) => {
     if (error) throw error;
     res.redirect('/');
   });
